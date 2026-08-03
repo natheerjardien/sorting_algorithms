72aed753f8d0a7d42840007673c5bea2291332bf
natheerjardien <st10435542@vcconnect.edu.za>
2026-08-03 18:30:17 +0200

    Push

---
+++ prInsertionSort/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace prInsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Unsorted Array
+            int[] numbers = { 12, 11, 13, 5, 6 };
+
+            Console.WriteLine("Original array:");
+            PrintArray(numbers);
+
+            InsertionSort(numbers);
+
+            Console.WriteLine("\nSorted Array: \n");
+            PrintArray(numbers);
+        }
+
+        static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // Start at teh second element
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i]; // The elemt we are currently trying to place
+                int j = i - 1;
+
+                // Move elements that are greater than the key to one position ahead of theirs
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j]; // Shift the larger element to the right
+                    j = j - 1; // Move the next elemnt to the left
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int val in arr)
+            {
+                Console.WriteLine(val + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ prjSortingAlgorithms/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace prjSortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bubble sort
+
+            // Unsorted Array
+            int[] array = { 64, 34, 25, 12, 11, 22, 90 };
+
+            Console.WriteLine("Unsorted Array");
+            PrintArray(array);
+
+            BubbleSort(array);
+
+            Console.WriteLine("\n Sorted Array");
+            PrintArray(array);
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+
+            // Outer loop for the number of passes
+            for (int i = 0; i < n -1; n++)
+            {
+                swapped = false;
+
+                // Inner loop to compare the adjacent elements
+                for (int j = 0; j < n -1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        swapped = true;
+                    }
+                }
+
+                // If no swaps occured, the area is already sorted
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
